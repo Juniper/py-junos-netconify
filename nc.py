e6a42864710d37120fdb4565f49cd9925763b2e6
Jeremy Schulman <jeremy@dark-matter.co>
2014-01-08 20:31:52 -0500

    updates

---
+++ nc.py
@@ -0,0 +1,4 @@
+from netconify import netconify, xmlmode_netconf
+
+tty = netconify(timeout=0.2)
+tty.login()
---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+sys.path.insert(0,'lib')
+from setuptools import setup, find_packages
+
+requirements = [ 'jinja2', 'lxml', 'pyserial']
+                 
+
+setup(
+    name = "junos-netconify",
+    namespace_packages = ['jnpr'],
+    version = "0.0.1",
+    author = "Jeremy Schulman",
+    author_email = "jschulman@juniper.net",
+    description = ( "Junos console/bootstrap automation" ),
+    license = "Apache 2.0",
+    keywords = "Junos NETCONF networking automation",
+    url = "http://www.github.com/jeremyschulman/py-junos-netconify",
+    package_dir={'':'lib'},    
+    packages=find_packages('lib'),
+    package_data={
+        'jnpr.junos.op': ['*.yml'],
+        'jnpr.junos.cfgro': ['*.yml']
+    },
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Other Scripting Engines',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Networking :: Firewalls',
+        'Topic :: Text Processing :: Markup :: XML'
+    ],
+)
