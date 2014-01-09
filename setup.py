83ee4f217b24491939250c5bdcde531968e1aa02
Jeremy Schulman <jeremy@dark-matter.co>
2014-01-09 12:59:57 -0500

    updates

--- setup.py
+++ setup.py
@@ -19,10 +19,6 @@ setup(
     url = "http://www.github.com/jeremyschulman/py-junos-netconify",
     package_dir={'':'lib'},    
     packages=find_packages('lib'),
-    package_data={
-        'jnpr.junos.op': ['*.yml'],
-        'jnpr.junos.cfgro': ['*.yml']
-    },
     install_requires=requirements,
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -33,14 +29,12 @@ setup(
         'Intended Audience :: Telecommunications Industry',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-        'Programming Language :: Other Scripting Engines',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking',
-        'Topic :: System :: Networking :: Firewalls',
         'Topic :: Text Processing :: Markup :: XML'
     ],
 )
