de8753cd39fd14aae43d7707f51720359380b023
Jeremy Schulman <nwkautomaniac@gmail.com>
2014-06-17 13:05:41 -0400

    updates to setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,38 @@ import os, sys
 from glob import glob
 from setuptools import setup, find_packages
 
-requirements = [ 'jinja2','pyserial','lxml' ]
+requirements = [ 'pyserial','lxml' ]
 
 setup(
-    name = "netconify",
-    version = "0.0.1",
+    name = "junos-netconify",
+    version = "0.1.0",
     author = "Jeremy Schulman",
-    author_email = "nwkautomaniac@gmain.com",
+    author_email = "nwkautomaniac@gmail.com",
     description = ( "Junos console/bootstrap automation"),
     license = "Apache 2.0",
-    keywords = "Junos NETCONF NOOB automation networking",
-    url = "http://www.github.com/jeremyschulman/py-junos-netconify",
+    keywords = "Junos NETCONF basic CONSOLE automation",
+    url = "http://www.github.com/Juniper/py-junos-netconify",
     install_requires=requirements,
     packages=find_packages('lib'),
     package_dir={'':'lib'},
     scripts=['tools/netconify'],
-    data_files=[
-      ('/etc/netconify/skel', glob('etc/skel/*.conf')),
-      ('/etc/netconify', ['etc/hosts'])
-    ]
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Other Scripting Engines',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Networking :: Firewalls',
+        'Topic :: Text Processing :: Markup :: XML'
+    ],    
 )
