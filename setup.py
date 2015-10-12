daf5c7c479d5ca14a7f7cea62a82aba86a531c6d
Pat <r2600r@gmail.com>
2015-10-12 10:49:27 -0500

    Correcting version

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
-import os, sys
+import os
+import sys
 from glob import glob
 from setuptools import setup, find_packages
 
-requirements = [ 'pyserial','lxml','jinja2' ]
+requirements = ['pyserial', 'lxml']
 
 setup(
-    name = "junos-netconify",
-    version = "0.1.1",
-    author = "Jeremy Schulman",
-    author_email = "jnpr-community-netdev@juniper.net",
-    description = ( "Junos console/bootstrap automation"),
-    license = "Apache 2.0",
-    keywords = "Junos NETCONF basic CONSOLE automation",
-    url = "http://www.github.com/Juniper/py-junos-netconify",
+    name="junos-netconify",
+    version="1.0.2",
+    author="Jeremy Schulman",
+    author_email="jnpr-community-netdev@juniper.net",
+    description=("Junos console/bootstrap automation"),
+    license="Apache 2.0",
+    keywords="Junos NETCONF basic CONSOLE automation",
+    url="http://www.github.com/Juniper/py-junos-netconify",
     install_requires=requirements,
     packages=find_packages('lib'),
-    package_dir={'':'lib'},
+    package_dir={'': 'lib'},
     scripts=['tools/netconify'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
@@ -28,11 +29,14 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Other Scripting Engines',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking',
         'Topic :: Text Processing :: Markup :: XML'
-    ],    
+    ],
 )
+
+
